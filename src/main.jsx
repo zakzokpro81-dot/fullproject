9de4300e4e92f7f79ee0a1ade99ab7db851db8f9
zakzokpro81-dot <zakzokpro81@gmail.com>
2026-01-15 12:05:01 +0300

    create project

---
+++ src/main.jsx
@@ -0,0 +1,21 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Router } from "@mui/icons-material";
+import App from "./App";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
