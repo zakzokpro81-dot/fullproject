b14fdb55e3f27852e2fcad8eb1ebb1e4b1c4ec33
zakzokpro <zakzokpro81@gmail.com>
2026-02-07 18:44:05 +0300

    add gap

--- ceptek/src/features/products/ProductList.jsx
+++ ceptek/src/features/products/ProductList.jsx
@@ -190,7 +190,13 @@ export function ProductsList() {
       <Paper sx={{ p: 2, mb: 3, borderRadius: 2 }}>
         <Stack
           spacing={1}
-          sx={{ mb: 3, flexDirection: { xs: "clumon", sm: "row" } }}
+          sx={{
+            mb: 3,
+            flexDirection: { xs: "clumon", sm: "row" },
+            alignItems: "center",
+            justifyContent: "space-between",
+            gap: 2,
+          }}
         >
           <Typography variant="h5" sx={{ mb: 2, flex: 1, fontWeight: "bold" }}>
             Inventory Management
